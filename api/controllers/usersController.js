ba494f3eee13306bb950f6e49e2cc57b11c8155a
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2018-06-17 22:17:52 +0200

    login route added

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 /*------------------------------------------------------------------*/
@@ -42,7 +42,48 @@ exports.user_signup = (req, res, next) =>{
 
 /*------------------------------------------------------------------*/
 exports.user_login = (req, res, next) => {
-  res.status(200).send('login working');
+  User.find({email: req.body.email}).exec()
+  .then(user => {
+    if (user.length < 1) {
+      return res.status(401).json({
+        message:'Auth failed'
+      });
+    }
+    bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+      if(err){
+        return res.status(401).json({
+          message:'Auth failed'
+        });
+      }
+      if (result) {
+        const token = jwt.sign({
+          email: user[0].email,
+          userId: user[0]._id
+        },
+        process.env.secret,
+        {
+          expiresIn:"1h"
+        });
+        return res.status(200).json({
+          message: 'Auth successful',
+          user:{
+            username: user[0].username,
+            email:user[0].email
+          },
+          token: token
+        });
+      }
+      res.status(401).json({
+        message:'Auth failed'
+      });
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error:err
+    });
+  });
 }
 
 /*------------------------------------------------------------------*/
