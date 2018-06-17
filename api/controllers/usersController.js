62344c8534d2b440187d5d3fdbd576389c298544
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2018-06-17 19:10:12 +0200

    delete user route added

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -46,6 +46,17 @@ exports.user_login = (req, res, next) => {
 }
 
 /*------------------------------------------------------------------*/
- exports.user_delete = (req, res, next) => {
-   res.status(200).send('delete working');
- }
+exports.user_delete = (req, res, next) => {
+  User.remove({_id: req.params.userId}).exec()
+  .then(result =>{
+    res.status(200).json({
+      message:'user deleted'
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error:err
+    });
+  });
+}
