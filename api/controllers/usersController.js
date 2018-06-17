4da93ba77a3a9e1940c35243a6d80de079413737
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2018-06-17 14:43:11 +0200

    signup added to routes

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,9 +1,43 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const User = require("../models/user");
 
 /*------------------------------------------------------------------*/
 exports.user_signup = (req, res, next) =>{
-  res.status(200).send('signup working');
+  User.find({ email: req.body.email }).exec()
+    .then(user => {
+      if(user.length >= 1){
+        return res.status(409).json({
+          message: 'email exist'
+        });
+      } else {
+
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if(err){
+            return res.status(500).json({error:err});
+          }else{
+            const user = new User({
+              username: req.body.username,
+              email: req.body.email,
+              password: hash
+            });
+
+            user.save()
+              .then(result => {
+                console.log(result);
+                res.status(201).json({ message: 'user created' });
+              })
+              .catch(err => {
+                console.log(err);
+                res.status(500).json({
+                  error:err
+                });
+              });
+          }
+        })
+      }
+    })
 }
 
 /*------------------------------------------------------------------*/
