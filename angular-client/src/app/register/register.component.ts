3bc9a5d5002b686439b76a4b244b52c8977ee501
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2018-06-19 22:47:02 +0200

    register added / front

--- angular-client/src/app/register/register.component.ts
+++ angular-client/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm:FormGroup;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'username': new FormControl(null, [Validators.required]),
+      'password' : new FormControl(null, [Validators.required])
+    })
+  }
+
+  register(){
+    const user= this.registerForm.value;
+    //console.log(user);
+
+    this.authService.register(user).subscribe(
+      (result)=>{
+        console.log(result)
+        this.router.navigate(['/login']);
+      },
+      (err)=>{
+        if(err){
+          console.log(err.status)
+        }
+      }
+    );
   }
 
 }
