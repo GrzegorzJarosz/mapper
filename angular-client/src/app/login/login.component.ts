04cbd7519ff2de540f92727237ec9de2d18d52a6
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2018-06-20 20:21:09 +0200

    login added / front

--- angular-client/src/app/login/login.component.ts
+++ angular-client/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm:FormGroup;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  user;
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'password' : new FormControl(null, [Validators.required])
+    });
+  }
+
+  login(){
+    const user = this.loginForm.value;
+    //console.log(user);
+
+    this.authService.authenticate(user).subscribe(
+      (result)=>{
+        // console.log(result);
+        this.authService.localUserStore(result);
+        this.router.navigate(['/']);
+      },
+      (err)=>{
+        if(err){
+        console.log(err.status)
+        }
+      }
+    );
   }
 
 }
