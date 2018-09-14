953dba54f7eadd858e1421ae1db09658b7850472
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2018-09-14 22:28:45 +0200

    methods for places moved to controllers

--- api/controllers/placesController.js
+++ api/controllers/placesController.js
@@ -1,2 +1,38 @@
 const mongoose = require("mongoose");
 const Place = require("../models/place");
+
+/*------------------------------------------------------------------*/
+exports.add_place = (req,res) => {
+  const place = new Place({
+    lat:req.body.lat,
+    lng:req.body.lng,
+    description:req.body.description,
+    user:req.params.user,
+    date: Date.now()
+  });
+  place.save()
+  .then(result => {
+    res.status(200).json({msg:'new place created'})
+  })
+  .catch(err => {
+    console.log('saving error');
+    res.status(500).json({error: 'some problems with saving'})}
+  );
+}
+
+/*------------------------------------------------------------------*/
+exports.delete_place = (req,res) => {
+  Place.remove({_id: req.params.id}).exec().then(
+    (result) => {
+      res.status(200).json({msg:'place succesfully deleted'})
+    })
+    .catch(err => {
+      console.log('deleting error');
+      res.status(500).json({error: 'some problems with deleting'})}
+    );
+}
+
+/*------------------------------------------------------------------*/
+exports.getall_places = (req,res) => {
+  Place.find({user:req.params.user}).exec().then( places => res.send(places))
+}
