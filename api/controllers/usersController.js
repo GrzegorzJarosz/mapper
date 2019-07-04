2cb479c5b318d92533a087e384975e685b0d4965
Grzegorz Jarosz <grzegorz82jarosz@gmail.com>
2019-07-04 21:22:14 +0200

    username on signup uniqe(back)

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -8,40 +8,52 @@ exports.user_signup = (req, res, next) => {
   User.find({
       email: req.body.email
     }).exec()
-    .then(user => {
-      if (user.length >= 1) {
+    .then(user1 => {
+      if (user1.length >= 1) {
         return res.status(409).json({
           message: 'email exist'
         });
       } else {
-
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
-            const user = new User({
-              username: req.body.username,
-              email: req.body.email,
-              password: hash
-            });
-
-            user.save()
-              .then(result => {
-                res.status(201).json({
-                  message: 'user created'
-                });
-              })
-              .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                  // error:err
-                  message: 'Problems with saving, please check your form and try again'
-                });
+        User.find({
+            username: req.body.username
+          }).exec()
+          .then((user2) => {
+            if (user2.length >= 1) {
+              return res.status(409).json({
+                message: 'username exist'
               });
-          }
-        })
+            } else {
+              bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                  return res.status(500).json({
+                    error: err
+                  });
+                } else {
+                  const user = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: hash
+                  });
+
+                  user.save()
+                    .then(result => {
+                      res.status(201).json({
+                        message: 'user created'
+                      });
+                    })
+                    .catch(err => {
+                      console.log(err);
+                      res.status(500).json({
+                        // error:err
+                        message: 'Problems with saving, please check your form and try again'
+                      });
+                    });
+                }
+              })
+            }
+          })
+
+
       }
     })
 }
